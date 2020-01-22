8b5afa18d54f7c134cbfe641bba70e546fb53f06
Yeauty <YeautyYE@gmail.com>
2020-01-22 16:22:19 +0800

    get rid of useless annotation

--- src/main/java/org/yeauty/autoconfigure/NettyWebSocketAutoConfigure.java
+++ src/main/java/org/yeauty/autoconfigure/NettyWebSocketAutoConfigure.java
@@ -1,10 +1,7 @@
 package org.yeauty.autoconfigure;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.yeauty.annotation.EnableWebSocket;
-import org.yeauty.standard.ServerEndpointExporter;
 
 @EnableWebSocket
-@ConditionalOnMissingBean(ServerEndpointExporter.class)
 public class NettyWebSocketAutoConfigure {
 }
